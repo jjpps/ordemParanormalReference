0608c58eafeb11c3a1ea4a3129d1224a3187b035
jpps <joao.pedro@labsit.io>
2023-10-17 00:31:04 -0300

    box with hover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,29 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
+      <br></br>
       <div className="container mx-auto">
         <div className="grid grid-rows-1 grid-flow-col gap-1">
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
+          <div>
+            <div className="box-border h-48 w-48 rounded hover:rounded-md border-4 hover:border-sky-200  ">
+              <div className="h-full w-full "></div>
+            </div>
+          </div>
+          <div>
+            <div className="box-border h-48 w-48 rounded hover:rounded-md border-4 hover:border-sky-200  ">
+              <div className="h-full w-full "></div>
+            </div>
+          </div>
+          <div>
+            <div className="box-border h-48 w-48 rounded hover:rounded-md border-4 hover:border-sky-200  ">
+              <div className="h-full w-full "></div>
+            </div>
+          </div>
+          <div>
+            <div className="box-border h-48 w-48 rounded hover:rounded-md border-4 hover:border-sky-200  ">
+              <div className="h-full w-full "></div>
+            </div>
+          </div>
         </div>
       </div>
     </main>
