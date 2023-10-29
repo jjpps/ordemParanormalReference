d515f1d3f9c9bccf8c533aede05b849d90596a61
Rodrigo <rodrigo.a42@gmail.com>
2023-10-29 18:40:36 -0300

    CharacterData_incio

--- src/app/CharacterPage.tsx
+++ src/app/CharacterPage.tsx
@@ -2,14 +2,17 @@ import Link from "next/link";
 
 export function CharacterPage() {
   return (
-    <div>
-      <Link href="/CharacterData">Link para Charecter</Link>
+    <Link
+      href={{
+        pathname: "/CharacterData",
+      }}
+    >
       <div
         className="bg-scroll h-[500px] w-[500px] bg-center bg-[length:298px] bg-no-repeat grayscale hover:grayscale-0 transition duration-[1000ms]  "
         style={{
           backgroundImage: `url("miriam.png")`,
         }}
       ></div>
-    </div>
+    </Link>
   );
 }
